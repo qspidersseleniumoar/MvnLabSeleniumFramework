1fe90c4b8fd414375521f0d503d2bf10d72b041a
Administrator <Administrator@VI06ONBNI06F7>
2018-03-02 03:19:49 -0800

    commit

---
+++ MvnLabProject/src/test/java/com/qspiders/test/Sample2Test.java
@@ -0,0 +1,14 @@
+package com.qspiders.test;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+	
+	@Test
+	public void sample2test() {
+		
+	System.out.println("execute 2 class");
+
+	}
+
+}
